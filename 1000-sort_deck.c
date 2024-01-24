f14c2523b1fe65c6a03c318f9d9eeec718d4553f
Kingsley <louis.officialz@gmail.com>
2024-01-23 18:27:55 -0800

    *.c

---
+++ 1000-sort_deck.c
@@ -0,0 +1,128 @@
+#include "deck.h"
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: String 1
+ * @s2: String 2
+ *
+ * Return: 0 if equal
+ */
+int _strcmp(char *s1, const char *s2)
+{
+	while (*s1 != '\0')
+	{
+		if (*s2 == '\0')
+			return (*s1);
+		if (*s2 > *s1)
+			return (*s1 - *s2);
+		if (*s1 > *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s2 != '\0')
+		return (*s2);
+	return (0);
+}
+/**
+ * _swap - Swaps two nodes of doubly linked list
+ *
+ * @node: Node base to change
+ * @deck: Double link list head
+ *
+ * Return: No Return
+ */
+void _swap(deck_node_t **node, deck_node_t **deck)
+{
+	deck_node_t *temp = *node, *temp2, *temp3;
+
+	if (!(*node)->prev)
+		*deck = (*node)->next;
+
+	temp = temp3 = *node;
+	temp2 = temp->next;
+
+	temp->next = temp2->next;
+	temp3 = temp->prev;
+	temp->prev = temp2;
+	temp2->next = temp;
+	temp2->prev = temp3;
+
+	if (temp2->prev)
+		temp2->prev->next = temp2;
+
+
+	if (temp->next)
+		temp->next->prev = temp;
+
+	*node = temp2;
+}
+/**
+ * CardValue - Obtains poker card Value from node
+ *
+ * @cardNode: node of poker card
+ *
+ * Return: Value between 1 and 52
+ */
+int CardValue(deck_node_t *cardNode)
+{
+	char *cardnum[13] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+			     "Jack", "Queen", "King"};
+	int value, i;
+	const char *cnum;
+
+	cnum = cardNode->card->value;
+	for (i = 0; i < 13;  i++)
+		if (!_strcmp(cardnum[i], cnum))
+		{
+			i++;
+			break;
+		}
+
+	value = i + (cardNode->card->kind) * 13;
+
+	return (value);
+}
+/**
+ * sort_deck - Sorts a poker cards deck
+ *
+ * @deck: Doubly linked list
+ *
+ * Return: No Return
+ */
+void sort_deck(deck_node_t **deck)
+{
+	deck_node_t *head, *tback, *aux;
+
+	if (!deck || !(*deck) || (!((*deck)->prev) && !((*deck)->next)))
+		return;
+
+	head = *deck;
+	while (head && head->next)
+	{
+		if (CardValue(head) > CardValue(head->next))
+		{
+			aux = head;
+
+			_swap(&aux, deck);
+			head = aux;
+			tback = aux;
+
+			while (tback && tback->prev)
+			{
+				if (CardValue(tback) < CardValue(tback->prev))
+				{
+					aux = tback->prev;
+
+					_swap(&(aux), deck);
+
+					tback = aux->next;
+				}
+
+				tback = tback->prev;
+			}
+		}
+		head = head->next;
+	}
+}
